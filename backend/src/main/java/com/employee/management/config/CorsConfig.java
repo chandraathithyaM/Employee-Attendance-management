45864e6acc73c3cba6a1654fd511b1093310fd19
chandraathithyaM <chandraathithyam@gmail.com>
2026-03-25 10:50:43 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/employee/management/config/CorsConfig.java
+++ backend/src/main/java/com/employee/management/config/CorsConfig.java
@@ -3,7 +3,6 @@ package com.employee.management.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.*;
-
 import java.util.List;
 
 @Configuration
@@ -11,21 +10,19 @@ public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-
         CorsConfiguration config = new CorsConfiguration();
-
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "https://employee-attendance-management-beryl.vercel.app"
         ));
-
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("Authorization"));
         config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
         return source;
     }
 }
