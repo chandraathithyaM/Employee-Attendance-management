9a8fc3292dcab8a9984925719cd9b1c51bee6425
chandraathithyaM <chandraathithyam@gmail.com>
2026-03-25 09:49:22 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://employee-attendance-management-production.up.railway.app/api/auth/google';
+const API_BASE_URL = 'https://employee-attendance-management-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
