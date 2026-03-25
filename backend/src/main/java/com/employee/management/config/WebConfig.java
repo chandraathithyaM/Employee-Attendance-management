661d2c423eb620d7b052314c96401365efdaeee4
chandraathithyaM <chandraathithyam@gmail.com>
2026-03-25 09:54:36 +0530

    Update WebConfig.java

--- backend/src/main/java/com/employee/management/config/WebConfig.java
+++ backend/src/main/java/com/employee/management/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                 .allowedOriginPatterns("https://*.vercel.app","http://localhost:5173")
+                 .allowedOriginPatterns("https://employee-attendance-management-production.up.railway.app","http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
